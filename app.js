ab945f336619f8f5ab27bc836296fd4dbd8ff04f
p7pjbrjpyy-collab <p7pjbrjpyy@privaterelay.appleid.com>
2026-07-06 23:04:36 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+// ===============================
+// My Gym
+// Version 1.0
+// ===============================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            alert("This section is coming soon!");
+
+        });
+
+    });
+
+});
