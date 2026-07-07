9afbf86cccfc4f94fa3177f0e1a6fe3ad9fbc366
James Cobb <jamesmcobb@hotmail.co.uk>
2026-07-07 22:59:00 +0100

    Sprint 2.1 - Reorganise project structure

---
+++ js/app.js
@@ -0,0 +1,129 @@
+// =================================
+// Gym Tracker
+// Version 0.3
+// Workout Screen
+// =================================
+
+
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+
+
+        const startWorkoutButton =
+            document.getElementById("startWorkout");
+
+
+        startWorkoutButton.addEventListener(
+            "click",
+            () => {
+
+
+                showWorkout();
+
+
+            }
+        );
+
+
+    }
+);
+
+
+
+function showWorkout() {
+
+
+    const container =
+        document.querySelector(".container");
+
+
+    let workoutHTML = `
+
+        <header>
+
+            <h1>💪 Today's Workout</h1>
+
+            <p class="subtitle">
+                Let's get started James
+            </p>
+
+        </header>
+
+        <main>
+
+    `;
+
+
+
+    currentWorkout.forEach(
+        exercise => {
+
+
+            workoutHTML += `
+
+                <div class="exercise-card">
+
+                    <h2>
+                        ${exercise.name}
+                    </h2>
+
+
+                    <p>
+                        <strong>Target:</strong>
+                        ${exercise.sets} × ${exercise.reps}
+                    </p>
+
+
+                    <p>
+                        <strong>Weight:</strong>
+                        ${exercise.weight}
+                    </p>
+
+
+                    <p>
+                        <strong>Rest:</strong>
+                        ${exercise.rest}
+                    </p>
+
+
+                </div>
+
+            `;
+
+
+        }
+    );
+
+
+
+    workoutHTML += `
+
+        </main>
+
+        <button id="backHome">
+            ← Back Home
+        </button>
+
+    `;
+
+
+
+    container.innerHTML =
+        workoutHTML;
+
+
+
+    document
+        .getElementById("backHome")
+        .addEventListener(
+            "click",
+            () => {
+
+                location.reload();
+
+            }
+        );
+
+
+}
