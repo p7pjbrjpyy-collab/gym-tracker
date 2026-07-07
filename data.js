2e963d334927fdbcdec380b701decdbafe6434ee
James Cobb <jamesmcobb@hotmail.co.uk>
2026-07-07 19:20:19 +0100

    Version 0.3 - Add workout screen and exercise data

---
+++ data.js
@@ -0,0 +1,109 @@
+// =================================
+// Gym Tracker
+// Workout Data
+// Version 0.3
+// =================================
+
+
+const currentWorkout = [
+
+    {
+        id: 1,
+        name: "Row (Warm-up)",
+        sets: 1,
+        reps: "1500 m",
+        weight: "-",
+        rest: "-"
+    },
+
+
+    {
+        id: 2,
+        name: "Lat Pulldown",
+        sets: 2,
+        reps: 30,
+        weight: "30 kg",
+        rest: "30 sec"
+    },
+
+
+    {
+        id: 3,
+        name: "DB Bench Press",
+        sets: 4,
+        reps: 12,
+        weight: "15 kg each",
+        rest: "30 sec"
+    },
+
+
+    {
+        id: 4,
+        name: "Shoulder Press (DB)",
+        sets: 3,
+        reps: 10,
+        weight: "15 kg",
+        rest: "60 sec"
+    },
+
+
+    {
+        id: 5,
+        name: "Pec Fly",
+        sets: 4,
+        reps: 12,
+        weight: "7 kg",
+        rest: "30 sec"
+    },
+
+
+    {
+        id: 6,
+        name: "Low Row",
+        sets: 4,
+        reps: 12,
+        weight: "30 kg",
+        rest: "45 sec"
+    },
+
+
+    {
+        id: 7,
+        name: "Single-Leg Leg Extension",
+        sets: 4,
+        reps: 12,
+        weight: "20 kg",
+        rest: "Continuous"
+    },
+
+
+    {
+        id: 8,
+        name: "Goblet Squat",
+        sets: 3,
+        reps: 12,
+        weight: "10 kg kettlebell",
+        rest: "30 sec"
+    },
+
+
+    {
+        id: 9,
+        name: "Split Squat",
+        sets: 3,
+        reps: 12,
+        weight: "10 kg kettlebell",
+        rest: "30 sec"
+    },
+
+
+    {
+        id: 10,
+        name: "Mobility",
+        sets: "-",
+        reps: "-",
+        weight: "-",
+        rest: "-"
+    }
+
+];
